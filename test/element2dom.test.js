b7466e2324247a5df7dc57d5b22b037a7b94d275
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2022-09-02 19:25:53 +0200

    Make element2dom.tests actually usefull

--- test/element2dom.test.js
+++ test/element2dom.test.js
@@ -1,13 +1,36 @@
 /**
  * @jest-environment jsdom
  */
-import {dom2XElement, XElement2Dom} from "../dist/esm/xml-api.js";
+import {XElement2Dom} from "../dist/esm/xml-api.js";
 
 
-const xml = "<xml><!-- comment -->text<elem attr1='attr2' attr3='attr4'></elem></xml>"
+const xml = {
+    "type": "Element",
+    "content": [
+        {"type": "Comment", "text": " comment "},
+        {"type": "Text", "text": "text"},
+        {"type": "Element",
+        "content": [
+            {"type": "Attribute", "name": "attr1", "value": "attr2"},
+            {"type": "Attribute", "name": "attr3", "value": "attr4"}],
+        "name": "elem"
+        },
+        {"type": "CDATA", "text": "Inhalt"}],
+    "name": "xml"
+}
+
 
 test("parse simple xml", async () => {
-    const document = new window.DOMParser().parseFromString(xml, "application/xml");
-    const root = document.children.item(0);
-    console.log(XElement2Dom(dom2XElement(root)).outerHTML);
+    const result = XElement2Dom(xml);
+
+    expect(result.nodeType).toBe(1);
+    expect(result.childNodes.item(0).nodeType).toBe(8);
+    expect(result.childNodes.item(0).textContent).toBe(" comment ");
+    expect(result.childNodes.item(1).nodeType).toBe(3);
+    expect(result.childNodes.item(1).textContent).toBe("text");
+    expect(result.childNodes.item(2).nodeType).toBe(1);
+    expect(result.childNodes.item(2).attributes.length).toBe(2);
+    expect(result.childNodes.item(3).nodeType).toBe(4);
+    expect(result.childNodes.item(3).textContent).toBe("Inhalt");
+
 });
