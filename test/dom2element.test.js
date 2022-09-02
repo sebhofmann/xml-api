0b3ee5d8d2f0a85574d8134e7c53f593e0d31bd3
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2022-09-02 19:14:51 +0200

    Make dom2element test actually usefull

--- test/dom2element.test.js
+++ test/dom2element.test.js
@@ -5,11 +5,21 @@ import {dom2XElement} from "../dist/esm/xml-api.js";
 
 
 
-const xml = "<xml><!-- comment -->text<elem attr1='attr2' attr3='attr4'></elem></xml>"
+const xml = "<xml><!-- comment -->text<elem attr1='attr2' attr3='attr4'></elem><![CDATA[Inhalt]]></xml>"
 
 test("parse simple xml", async () => {
     const document = new window.DOMParser().parseFromString(xml, "application/xml");
     const root = document.children.item(0);
-    console.log(dom2XElement(root))
+    const result = dom2XElement(root);
 
+    expect(result.type).toBe("Element");
+    expect(result.content.length).toBe(4);
+    expect(result.content[0].type).toBe("Comment");
+    expect(result.content[0].text).toBe(" comment ");
+    expect(result.content[1].type).toBe("Text");
+    expect(result.content[1].text).toBe("text");
+    expect(result.content[2].type).toBe("Element");
+    expect(result.content[2].name).toBe("elem");
+    expect(result.content[3].type).toBe("CDATA");
+    expect(result.content[3].text).toBe("Inhalt");
 });
